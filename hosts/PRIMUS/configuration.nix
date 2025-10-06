c039e5c5845bf8fe2314be17f3a827ed98ff8182
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2025-10-06 18:27:41 -0400

    Enable flatpak

--- hosts/PRIMUS/configuration.nix
+++ hosts/PRIMUS/configuration.nix
@@ -196,6 +196,16 @@
     cosmic-store
   ];
 
+  # Need flatpak for some packages (particularly COSMIC DE)
+  services.flatpak.enable = true;
+  systemd.services.flatpak-repo = {
+    wantedBy = [ "multi-user.target" ];
+    path = [ pkgs.flatpak ];
+    script = ''
+      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    '';
+  };
+
   # Run dynamically linked binaries
   programs.nix-ld.enable = true;
   programs.nix-ld.libraries = with pkgs; [

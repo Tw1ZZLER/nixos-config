50109f2cb3cb11e5d05ba84b8f0b74facd54428e
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2025-10-08 15:37:15 -0400

    Carla and putting plugins into REAPER

--- hosts/PRIMUS/home.nix
+++ hosts/PRIMUS/home.nix
@@ -220,6 +220,7 @@
     # An extra line later is needed to actually link ReaPack into ~/.config/REAPER/UserPlugins/
     reaper # WARN: Proprietary
     reaper-reapack-extension
+    carla # Audio plugin host
 
     # Video stuff
     obs-studio
@@ -251,7 +252,15 @@
 
   # Link ReaPack to Reaper User Plugins
   home.file.".config/REAPER/UserPlugins/reaper_reapack-x86_64.so" = {
-    source = pkgs.reaper-reapack-extension + "/UserPlugins/reaper_reapack-x86_64.so";
+    source = pkgs.unstable.reaper-reapack-extension + "/UserPlugins/reaper_reapack-x86_64.so";
+  };
+
+  # Link Carla Plugins to Reaper VST and LV2 Plugins
+  home.file.".vst/carla.vst" = {
+    source = pkgs.unstable.carla + "/lib/vst/carla.vst";
+  };
+  home.file.".lv2/carla.lv2" = {
+    source = pkgs.unstable.carla + "/lib/lv2/carla.lv2";
   };
 
   # Nicely reload system units when changing configs

e94637d2def15223f022cf2c48afc93917ce884b
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2025-10-10 17:36:58 -0400

    Plymouth

--- hosts/PRIMUS/configuration.nix
+++ hosts/PRIMUS/configuration.nix
@@ -28,6 +28,7 @@
     # Not defaults (changes per system)
     ../../modules/nixos/intel-graphics.nix
     ../../modules/nixos/cosmic.nix
+    ../../modules/nixos/plymouth.nix
   ];
 
   nixpkgs = {
@@ -82,37 +83,6 @@
 
   # Kernel
   boot.kernelPackages = pkgs.linuxPackages_latest;
-
-  # Plymouth / silent boot
-  boot = {
-    plymouth = {
-      enable = true;
-      theme = "rings";
-      logo = "${pkgs.nixos-icons}/share/icons/hicolor/128x128/apps/nix-snowflake.png";
-      themePackages = with pkgs; [
-        # By default we would install all themes
-        (adi1090x-plymouth-themes.override {
-          selected_themes = [ "rings" ];
-        })
-      ];
-    };
-
-    # Enable "Silent boot"
-    consoleLogLevel = 3;
-    initrd.verbose = false;
-    initrd.systemd.enable = true;
-    kernelParams = [
-      "quiet"
-      "splash"
-      "boot.shell_on_fail"
-      "udev.log_priority=3"
-      "rd.systemd.show_status=auto"
-    ];
-    # Hide the OS choice for bootloaders.
-    # It's still possible to open the bootloader list by pressing any key
-    # It will just not appear on screen unless a key is pressed
-    loader.timeout = 0;
-  };
 
   # System76 hardware
   hardware.system76.enableAll = true;

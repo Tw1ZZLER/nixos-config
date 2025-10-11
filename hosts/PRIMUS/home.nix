04ae8db82db87dfdd91fb8d0a9354dfcf4a001d5
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2025-10-11 01:32:47 -0400

    Link home.nix in its regular spot to our config

--- hosts/PRIMUS/home.nix
+++ hosts/PRIMUS/home.nix
@@ -18,6 +18,9 @@
     ../../modules/home-manager
   ];
 
+  # Link this home.nix to ~/.config/home-manager/home.nix
+  xdg.configFile."home-manager/home.nix".source = ./home.nix;
+
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs.unstable; [

cb50a3a9ee6154dfbf92314abb5d913d9805f68b
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2025-10-27 12:41:12 -0400

    Wget

--- hosts/PRIMUS/home.nix
+++ hosts/PRIMUS/home.nix
@@ -29,6 +29,9 @@
     # Zathura
     zathura
 
+    # Wget (why do I not have this by default?)
+    wget
+
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of

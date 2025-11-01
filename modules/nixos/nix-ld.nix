b53765bb7f4f6897d8c3caced1afcdf655c37fbb
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2025-10-31 22:08:11 -0400

    ICU should be here I usppose

--- modules/nixos/nix-ld.nix
+++ modules/nixos/nix-ld.nix
@@ -1,9 +1,10 @@
-{ ... }:
+{ pkgs, ... }:
 {
   # Run dynamically linked binaries
   programs.nix-ld.enable = true;
-  # programs.nix-ld.libraries = with pkgs; [
-  #   # add any missing dynamic libraries for unpackaged
-  #   # programs here, NOT in environment.systemPackages
-  # ];
+  programs.nix-ld.libraries = with pkgs; [
+    # add any missing dynamic libraries for unpackaged
+    # programs here, NOT in environment.systemPackages
+    unstable.icu
+  ];
 }

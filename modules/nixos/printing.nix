beac069116baa04c1b948576d7077e0b11d7c707
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2025-11-18 09:36:43 -0500

    Printing authentication works

--- modules/nixos/printing.nix
+++ modules/nixos/printing.nix
@@ -57,6 +57,9 @@
         location = "ONU";
         deviceUri = "smb://onuprinters.onu.edu/Black-and-White";
         model = "foomatic-db-ppds/KONICA_MINOLTA-bizhub_C352-Postscript-KONICA_MINOLTA.ppd.gz";
+        ppdOptions = {
+          AuthInfoRequired = "username,password";
+        };
       }
       {
         name = "onu-color";
@@ -64,6 +67,9 @@
         location = "ONU";
         deviceUri = "smb://onuprinters.onu.edu/Color";
         model = "foomatic-db-ppds/KONICA_MINOLTA-bizhub_C352-Postscript-KONICA_MINOLTA.ppd.gz";
+        ppdOptions = {
+          AuthInfoRequired = "username,password";
+        };
       }
     ];
     ensureDefaultPrinter = "onu-bw";

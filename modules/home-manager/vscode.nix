2b1b032d01c8d1918f1fb2e12968199467c33642
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2025-11-19 23:33:04 -0500

    Uh this works I guess

--- modules/home-manager/vscode.nix
+++ modules/home-manager/vscode.nix
@@ -1,8 +1,7 @@
 # Not the best text editor, but necessary in many cases
 { pkgs, ... }:
 {
-  programs.vscode = {
-    enable = true;
-    package = pkgs.unstable.vscode.fhs;
-  };
+  home.packages = with pkgs.unstable; [
+    vscode.fhs
+  ];
 }

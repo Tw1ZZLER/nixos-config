ccb29dd349a3de78ab03dc736c019788b3a8df48
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2025-12-01 22:33:29 -0500

    Whoops

--- modules/nixos/stylix.nix
+++ modules/nixos/stylix.nix
@@ -5,24 +5,24 @@
 
     # Catpuccin-frappe from the base16-schemes package
     base16Scheme = "${pkgs.unstable.base16-schemes}/share/themes/catppuccin-frappe.yaml";
-  };
 
-  fonts = {
-    serif = {
-      package = pkgs.unstable.nerd-fonts.overpass;
-      name = "Overpass Nerd Font";
-    };
-    sansSerif = {
-      package = pkgs.unstable.nerd-fonts.overpass;
-      name = "Overpass Nerd Font";
-    };
-    monospace = {
-      package = pkgs.unstable.nerd-fonts.victor-mono;
-      name = "VictorMono Nerd Font";
-    };
-    emoji = {
-      package = pkgs.unstable.noto-fonts-color-emoji;
-      name = "Noto Color Emoji";
+    fonts = {
+      serif = {
+        package = pkgs.unstable.nerd-fonts.overpass;
+        name = "Overpass Nerd Font";
+      };
+      sansSerif = {
+        package = pkgs.unstable.nerd-fonts.overpass;
+        name = "Overpass Nerd Font";
+      };
+      monospace = {
+        package = pkgs.unstable.nerd-fonts.victor-mono;
+        name = "VictorMono Nerd Font";
+      };
+      emoji = {
+        package = pkgs.unstable.noto-fonts-color-emoji;
+        name = "Noto Color Emoji";
+      };
     };
   };
 }

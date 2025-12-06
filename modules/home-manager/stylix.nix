86a8e3cb5240c635076a7d34b3497f140c7c8f50
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2025-12-06 14:23:50 -0500

    Stylix updated to work independently on home manager!

--- modules/home-manager/stylix.nix
+++ modules/home-manager/stylix.nix
@@ -1,13 +1,38 @@
 { pkgs, ... }:
 {
-  stylix.targets = {
-    neovim.enable = false;
-    starship.enable = false;
-    kde.enable = true;
-    gtk.enable = true;
-    zen-browser = {
-      enable = true;
-      profileNames = [ "tw1zzler" ];
+  stylix = {
+    enable = true;
+
+    # Catpuccin-frappe from the base16-schemes package
+    base16Scheme = "${pkgs.unstable.base16-schemes}/share/themes/catppuccin-frappe.yaml";
+
+    fonts = {
+      serif = {
+        package = pkgs.unstable.nerd-fonts.overpass;
+        name = "Overpass Nerd Font";
+      };
+      sansSerif = {
+        package = pkgs.unstable.nerd-fonts.overpass;
+        name = "Overpass Nerd Font";
+      };
+      monospace = {
+        package = pkgs.unstable.nerd-fonts.victor-mono;
+        name = "VictorMono Nerd Font";
+      };
+      emoji = {
+        package = pkgs.unstable.noto-fonts-color-emoji;
+        name = "Noto Color Emoji";
+      };
+    };
+    targets = {
+      neovim.enable = false;
+      starship.enable = false;
+      kde.enable = true;
+      gtk.enable = true;
+      zen-browser = {
+        enable = true;
+        profileNames = [ "tw1zzler" ];
+      };
     };
   };
 }

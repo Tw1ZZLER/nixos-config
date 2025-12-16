d1960ee0fb1b6b1d6e11535d1905a515efccb5b1
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2025-12-16 00:38:06 -0500

    Enormous update, required ocl to be removed bc it couldnt download

--- modules/nixos/intel-graphics.nix
+++ modules/nixos/intel-graphics.nix
@@ -8,7 +8,7 @@
     extraPackages = with pkgs; [
       vpl-gpu-rt # Intel Quick Sync Video (QSV)
       intel-compute-runtime # OpenCL
-      intel-ocl # Official proprietary OpenCL library # WARN: Proprietary
+      # intel-ocl # Official proprietary OpenCL library # WARN: Proprietary
       intel-media-driver # (VAAPI) Enable hardware acceleration with the Intel iHD driver
     ];
     extraPackages32 = with pkgs.pkgsi686Linux; [

9ab56219e412ff04f39107b050398749a4e49979
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2025-12-16 00:06:56 -0500

    Nixpkgs set to stable branch

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
     self.submodules = true;
 
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     # You can access packages and modules from different nixpkgs revs
     # at the same time. Here's an working example:
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";

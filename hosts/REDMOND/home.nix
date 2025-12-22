54b5b3abb61b5c5e1148633cb8076cbb5cd0bcbf
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2025-12-22 01:50:03 -0500

    Gnome keyring should be disabled here

--- hosts/REDMOND/home.nix
+++ hosts/REDMOND/home.nix
@@ -2,15 +2,7 @@
 # Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
 { pkgs, ... }:
 {
-  # You can import other home-manager modules here
   imports = [
-    # If you want to use modules your own flake exports (from modules/home-manager):
-    # outputs.homeManagerModules.example
-
-    # Or modules exported from other flakes (such as nix-colors):
-    # inputs.nix-colors.homeManagerModules.default
-
-    # You can also split up your configuration and import pieces of it here:
     ../../modules/nixpkgs.nix
   ];
 
@@ -23,6 +15,7 @@
   ark.enable = false;
   gwenview.enable = false;
   stylix-wrapper.enable = false;
+  gnome-keyring.enable = false;
 
   # Must pass hostname to Syncthing wrapper on non-NixOS hosts
   syncthing.hostName = "REDMOND";

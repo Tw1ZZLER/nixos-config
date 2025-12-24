e6944f51573bf7a740561e5ebb422b4742ff0f97
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2025-12-23 20:36:42 -0500

    Nix Index as a module

--- modules/nixos/programs/default.nix
+++ modules/nixos/programs/default.nix
@@ -1,8 +1,10 @@
 { lib, ... }:
 {
   imports = [
+    ./nix-index.nix
     ./weylus.nix
   ];
 
+  nix-index.enable = lib.mkDefault true;
   weylus.enable = lib.mkDefault true;
 }
---
+++ modules/nixos/programs/nix-index.nix
@@ -0,0 +1,22 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+{
+  options = {
+    nix-index.enable = lib.mkEnableOption "Enable nix-index tool for NixOS";
+  };
+  config = lib.mkIf config.nix-index.enable {
+    programs = {
+      nix-index = {
+        enable = true;
+        enableFishIntegration = true;
+      };
+
+      # Disable command-not-found globally for flake-based systems
+      command-not-found.enable = false;
+    };
+  };
+}

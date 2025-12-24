11efa87feb651c9474ce038cac05dd5e4a3cf9d1
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2025-12-24 00:13:32 -0500

    Try and fix nix-index again

--- modules/home-manager/programs/cli/nix-index.nix
+++ modules/home-manager/programs/cli/nix-index.nix
@@ -14,6 +14,14 @@
     nix-index.enable = lib.mkEnableOption "Enable nix-index tool for home-manager";
   };
   config = lib.mkIf config.nix-index.enable {
-    programs.nix-index.enable = true;
+    programs = {
+      nix-index = {
+        enable = true;
+        enableFishIntegration = true;
+      };
+
+      # Disable command-not-found globally for flake-based systems
+      command-not-found.enable = false;
+    };
   };
 }

a976b46f4a6b71333a57879f7a201c021d52ac91
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2025-12-31 06:39:32 -0500

    Make this an outOfStoreSymlink for live updating

--- modules/home-manager/programs/gui/wezterm.nix
+++ modules/home-manager/programs/gui/wezterm.nix
@@ -5,6 +5,9 @@
   config,
   ...
 }:
+let
+  dotfiles = config.lib.file.mkOutOfStoreSymlink "/home/tw1zzler/nixos-config/dotfiles";
+in
 {
   options = {
     wezterm.enable = lib.mkEnableOption "Enable Wezterm terminal emulator";
@@ -17,8 +20,8 @@
 
     # Wezterm configuration dotfiles
     xdg.configFile.wezterm = {
+      source = "${dotfiles}/wezterm";
       recursive = true;
-      source = ../../../../dotfiles/wezterm;
     };
   };
 }

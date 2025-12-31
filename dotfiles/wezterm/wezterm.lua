9ba0d65182c669deab141dfbcb6c61be41bb858d
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2025-12-31 06:43:23 -0500

    Yay for live updates

--- dotfiles/wezterm/wezterm.lua
+++ dotfiles/wezterm/wezterm.lua
@@ -12,15 +12,8 @@ config = {
 	font_size = 12,
 	color_scheme = "catppuccin-frappe",
 
-	colors = {
-		-- cursor_bg = "7aa2f7",
-		-- cursor_border = "7aa2f7",
-		-- background = "#1e1e2e",
-	},
-
 	enable_tab_bar = false,
-
-	window_background_opacity = 0.95,
+	window_background_opacity = 0.85,
 }
 
 -- Finally, return the configuration to wezterm:

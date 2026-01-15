d3c8281f984b1da550a10fe185133fefebc81270
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2026-01-15 05:36:36 -0500

    Adding direnv

--- modules/home-manager/programs/cli/default.nix
+++ modules/home-manager/programs/cli/default.nix
@@ -3,6 +3,7 @@
   imports = [
     ./bat.nix
     ./btop.nix
+    ./direnv.nix
     ./exercism.nix
     ./eza.nix
     ./fastfetch.nix
@@ -25,6 +26,7 @@
 
   bat.enable = lib.mkDefault true;
   btop.enable = lib.mkDefault true;
+  direnv.enable = lib.mkDefault true;
   exercism.enable = lib.mkDefault true;
   eza.enable = lib.mkDefault true;
   fastfetch.enable = lib.mkDefault true;
---
+++ modules/home-manager/programs/cli/direnv.nix
@@ -0,0 +1,22 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+
+{
+  options = {
+    direnv.enable = lib.mkEnableOption "Enable direnv, an environment switcher for the shell";
+  };
+
+  config = lib.mkIf config.direnv.enable {
+    programs.direnv = {
+      enable = true;
+      package = pkgs.unstable.direnv;
+      enableFishIntegration = true;
+      nix-direnv.enable = true;
+      nix-direnv.package = pkgs.unstable.nix-direnv;
+    };
+  };
+}

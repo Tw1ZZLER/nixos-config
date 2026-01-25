4c02aca10d6d02ebf16a2fa8f1d335495f0fa36d
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2026-01-25 11:28:35 -0500

    Btop settings

--- modules/home-manager/programs/cli/btop.nix
+++ modules/home-manager/programs/cli/btop.nix
@@ -13,6 +13,10 @@
     programs.btop = {
       enable = true;
       package = pkgs.unstable.btop;
+      settings = {
+        proc_tree = true;
+        theme_background = false;
+      };
     };
   };
 }

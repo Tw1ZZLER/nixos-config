d8bd2d1084707920c04eaf5ac743282de20d951a
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2026-01-27 21:54:47 -0500

    Capitalization

--- flake.nix
+++ flake.nix
@@ -15,11 +15,11 @@
     self.submodules = true;
 
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     # nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     # You can access packages and modules from different nixpkgs revs
     # at the same time. Here's an working example:
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     # Also see the 'unstable-packages' overlay at 'overlays/default.nix'.
 
     # Home manager

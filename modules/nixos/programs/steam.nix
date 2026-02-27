d740d485987d91831b28c49f01ae33b6193c9436
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2026-02-26 22:05:14 -0500

    Fixed black screen Steam on wayland

--- modules/nixos/programs/steam.nix
+++ modules/nixos/programs/steam.nix
@@ -12,7 +12,11 @@
   config = lib.mkIf config.steam.enable {
     programs.steam = {
       enable = true;
-      package = pkgs.unstable.steam;
+      package = pkgs.unstable.steam.override {
+        # Fixes black screen issues on Wayland
+        # https://github.com/Supreeeme/xwayland-satellite/issues/150
+        extraArgs = "-system-composer";
+      };
       protontricks = {
         enable = true;
         package = pkgs.unstable.protontricks;

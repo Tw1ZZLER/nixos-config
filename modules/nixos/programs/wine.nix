0f7d5cea5900ef9484ddff0c6a05185f9b401679
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2026-04-02 12:26:03 -0400

    Fix wine warning

--- modules/nixos/programs/wine.nix
+++ modules/nixos/programs/wine.nix
@@ -12,7 +12,7 @@
     # It's better to use unstable for this sort of thing
     environment.systemPackages = with pkgs.unstable; [
       # Wine
-      wineWowPackages.staging
+      wineWow64Packages.staging
       winetricks
       wineasio # JACK to ASIO driver
       (bottles.override { removeWarningPopup = true; }) # GUI for managing Wine prefixes

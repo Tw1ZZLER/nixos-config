baf6e53deb7c3d766ea23ab81d90f940e740157b
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2026-05-09 20:20:52 -0400

    This is not really necessary and causes errors

--- modules/nixos/services/flatpak.nix
+++ modules/nixos/services/flatpak.nix
@@ -11,12 +11,12 @@
   config = lib.mkIf config.flatpak.enable {
     # Need flatpak for some packages (particularly COSMIC DE)
     services.flatpak.enable = true;
-    systemd.services.flatpak-repo = {
-      wantedBy = [ "multi-user.target" ];
-      path = [ pkgs.flatpak ];
-      script = ''
-        flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-      '';
-    };
+    # systemd.services.flatpak-repo = {
+    #   wantedBy = [ "multi-user.target" ];
+    #   path = [ pkgs.flatpak ];
+    #   script = ''
+    #     flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    #   '';
+    # };
   };
 }

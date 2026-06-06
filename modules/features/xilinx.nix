014b053591280f934b96b82e4451f2a2612c689e
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2026-06-06 18:44:30 -0500

    Xilinx tools (need overlay, should add overlay in this file later)

--- modules/features/xilinx.nix
+++ modules/features/xilinx.nix
@@ -1,17 +1,11 @@
 # Xilinx tooling, including Vivado and Vitis
 # Uses overlay made available by MIT OpenCompute's nix-xilinx flake
 {
-  pkgs,
-  lib,
-  config,
+  self,
+  inputs,
   ...
-}:
-{
-  options = {
-    xilinx.enable = lib.mkEnableOption "Enable Xilinx tooling, including Vivado and Vitis";
-  };
-
-  config = lib.mkIf config.xilinx.enable {
+}: {
+  flake.homeModules.xilinx = {pkgs, ...}: {
     home.packages = with pkgs; [
       xilinx-shell
       vivado # WARN: Proprietary

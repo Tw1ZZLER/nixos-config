2ee46c76d95d94a41e4ec6c651fcf03eb39157ef
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2026-06-05 21:20:44 -0500

    xdg-user-dirs converted to dendritic

---
+++ modules/features/xdg-user-dirs.nix
@@ -0,0 +1,38 @@
+{
+  self,
+  inputs,
+  ...
+}: {
+  flake.homeModules.xdg-user-dirs =
+    # I like these user directories better because they are easy to type (and match more closely to the rest of the Linux filesystem)
+    {
+      lib,
+      config,
+      ...
+    }: {
+      options = {
+        xdg-user-dirs.enable = lib.mkEnableOption "Enable XDG user directories with custom paths";
+      };
+      config = lib.mkIf config.xdg-user-dirs.enable {
+        xdg = {
+          # Enable XDG
+          enable = true;
+
+          # Adjust XDG user directories
+          userDirs = {
+            enable = true;
+            desktop = "${config.home.homeDirectory}/desktop";
+            documents = "${config.home.homeDirectory}/doc";
+            download = "${config.home.homeDirectory}/dl";
+            music = "${config.home.homeDirectory}/media/music";
+            pictures = "${config.home.homeDirectory}/media/img";
+            publicShare = "${config.home.homeDirectory}/public";
+            templates = "${config.home.homeDirectory}/template";
+            videos = "${config.home.homeDirectory}/media/vid";
+            setSessionVariables = true;
+            createDirectories = true;
+          };
+        };
+      };
+    };
+}

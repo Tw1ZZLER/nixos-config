f3a6e2434871e72ebec80305d23db956b34daf79
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2026-06-05 20:26:34 -0500

    Zen browser added homemodule

--- modules/features/zen-browser.nix
+++ modules/features/zen-browser.nix
@@ -4,6 +4,54 @@
   inputs,
   ...
 }: {
+  flake.homeModules.zen-browser = {
+    pkgs,
+    lib,
+    ...
+  }: {
+    # Zen-browser as default browser
+    xdg.mimeApps = let
+      value = let
+        zen-browser = inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.beta; # or twilight
+      in
+        zen-browser.meta.desktopFileName;
+
+      associations = builtins.listToAttrs (
+        map
+        (name: {
+          inherit name value;
+        })
+        [
+          "application/x-extension-shtml"
+          "application/x-extension-xhtml"
+          "application/x-extension-html"
+          "application/x-extension-xht"
+          "application/x-extension-htm"
+          "x-scheme-handler/unknown"
+          "x-scheme-handler/mailto"
+          "x-scheme-handler/chrome"
+          "x-scheme-handler/about"
+          "x-scheme-handler/https"
+          "x-scheme-handler/http"
+          "application/xhtml+xml"
+          "application/json"
+          "text/plain"
+          "text/html"
+        ]
+      );
+    in {
+      associations.added = associations;
+      defaultApplications = associations;
+    };
+
+    # Zen Browser Program
+    programs.zen-browser = {
+      enable = true;
+      setAsDefaultBrowser = true;
+      package = self.packages.${pkgs.stdenv.hostPlatform.system}.myZen;
+    };
+  };
+
   perSystem = {pkgs, ...}: {
     packages.myZen = inputs.zen-browser.packages."${pkgs.stdenv.hostPlatform.system}".default.override {
       # POLICIES

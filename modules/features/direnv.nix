3701819d3fad266b08445cce6da48c0a6c5392f3
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2026-06-05 21:42:21 -0500

    Direnv converted to dendritic

---
+++ modules/features/direnv.nix
@@ -0,0 +1,25 @@
+{
+  self,
+  inputs,
+  ...
+}: {
+  flake.homeModules.direnv = {
+    pkgs,
+    lib,
+    config,
+    ...
+  }: {
+    options = {
+      direnv.enable = lib.mkEnableOption "Enable direnv, an environment switcher for the shell";
+    };
+
+    config = lib.mkIf config.direnv.enable {
+      programs.direnv = {
+        enable = true;
+        package = pkgs.unstable.direnv;
+        nix-direnv.enable = true;
+        nix-direnv.package = pkgs.unstable.nix-direnv;
+      };
+    };
+  };
+}

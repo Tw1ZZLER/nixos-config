7f37e07a84d81d40f7952af2293676c36b6dc5a7
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2026-06-06 19:27:53 -0500

    Overlays

--- flake.nix
+++ flake.nix
@@ -25,55 +25,8 @@
   #   let
   #     inherit (self) outputs;
   #     inherit (nixpkgs) lib;
-  #     # namespace = "tw1zzler"; # namespace for custom modules
-  #
-  #     # Supported systems for your flake packages, shell, etc.
-  #     systems = [
-  #       "aarch64-linux"
-  #       "i686-linux"
-  #       "x86_64-linux"
-  #       "aarch64-darwin"
-  #       "x86_64-darwin"
-  #     ];
-  #     # This is a function that generates an attribute by calling a function you
-  #     # pass to it, with each system as an argument
-  #     forAllSystems = lib.genAttrs systems;
-  #
   #   in
   #   {
-  #     # Your custom packages
-  #     # Accessible through 'nix build', 'nix shell', etc
-  #     packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
-  #     # Formatter for your nix files, available through 'nix fmt'
-  #     formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt);
-  #
-  #     # Your custom packages and modifications, exported as overlays
-  #     overlays = import ./overlays { inherit inputs; };
-  #     # Reusable nixos modules you might want to export
-  #     # These are usually stuff you would upstream into nixpkgs
-  #     nixosModules = import ./modules/nixos;
-  #     # Reusable home-manager modules you might want to export
-  #     # These are usually stuff you would upstream into home-manager
-  #     homeModules = import ./modules/home-manager;
-  #
-  #     # Development shell for NixOS configuration
-  #     devShells = forAllSystems (
-  #       system:
-  #       let
-  #         pkgs = nixpkgs.legacyPackages.${system};
-  #       in
-  #       {
-  #         default = pkgs.mkShell {
-  #           nativeBuildInputs = with pkgs; [
-  #             nil # lsp language server for nix
-  #             nixpkgs-fmt
-  #             sops
-  #             inputs.nixos-anywhere.packages.${system}.default
-  #           ];
-  #         };
-  #       }
-  #     );
-  #
   #     # NixOS configuration entrypoint
   #     # Available through 'nixos-rebuild --flake .#your-hostname'
   #     nixosConfigurations = {
@@ -120,19 +73,6 @@
   #       };
   #     };
   #
-  #     # Standalone home-manager configuration entrypoint
-  #     # Available through 'home-manager --flake .#your-username@your-hostname'
-  #     homeConfigurations = {
-  #       "tw1zzler@REDMOND" = inputs.home-manager.lib.homeManagerConfiguration {
-  #         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-  #         extraSpecialArgs = { inherit inputs outputs; };
-  #         modules = [
-  #           # > Our main home-manager configuration file <
-  #           ./hosts/REDMOND/home.nix
-  #         ];
-  #       };
-  #     };
-  #   };
 
     # Standalone home-manager configuration entrypoint
     # Available through 'home-manager --flake .#your-username@your-hostname'

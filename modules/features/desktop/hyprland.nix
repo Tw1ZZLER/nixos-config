bce20e4652c28159d637eb01b4b0c20b1f44834c
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2026-06-09 16:39:52 -0500

    hyprland

--- modules/features/desktop/hyprland.nix
+++ modules/features/desktop/hyprland.nix
@@ -3,6 +3,37 @@
   inputs,
   ...
 }: {
+  flake.nixosModules.hyprland = {pkgs, ...}: {
+    programs.hyprland = {
+      enable = true;
+      withUWSM = true; # to be honest I wish I knew why this doesn't work but I don't right now know!
+    };
+
+    # Portal configuration for screen sharing, file dialogs, etc.
+    xdg.portal = {
+      enable = true;
+      extraPortals = with pkgs; [
+        xdg-desktop-portal-gnome # Screen sharing portal
+        xdg-desktop-portal-gtk # Fallback portal for file dialogs and other interfaces
+      ];
+    };
+
+    environment = {
+      sessionVariables = {
+        # Wayland support for Electron and Chromium
+        NIXOS_OZONE_WL = "1";
+      };
+      systemPackages = with pkgs; [
+        # Other Wayland clipboard stuff
+        wl-clipboard
+        cliphist
+
+        # required for the default Hyprland config
+        kitty
+      ];
+    };
+  };
+
   # Links Hyprland configuration file
   flake.homeModules.hyprland = {config, ...}: let
     dotfiles = config.lib.file.mkOutOfStoreSymlink "/home/tw1zzler/nixos-config/dotfiles";

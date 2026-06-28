790c9649338df62b6dab409b233ec7866c85e62f
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2026-06-27 23:43:55 -0500

    Extra binary caches

--- modules/features/system/nix-wrapper.nix
+++ modules/features/system/nix-wrapper.nix
@@ -11,9 +11,19 @@
         # Enable flakes and new 'nix' command
         experimental-features = "nix-command flakes";
 
-        # Binary Cache
-        extra-substituters = ["https://noctalia.cachix.org"];
-        extra-trusted-public-keys = ["noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="];
+        # Extra Binary Caches
+        extra-substituters = [
+          "https://nix-community.cachix.org"
+          "https://hercules-ci.cachix.org"
+          "https://noctalia.cachix.org"
+          "https://catppuccin.cachix.org"
+        ];
+        extra-trusted-public-keys = [
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "hercules-ci.cachix.org-1:g99HiMR8Y9Z8mYicrKWW04daP0W5ZshhZ/pYgNfC+1U="
+          "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+          "catppuccin.cachix.org-1:noGdz9JST3clb8h9W9q6CZvGsDY6HG0cmM6b856w06M="
+        ];
       };
       extraOptions = ''
         warn-dirty = false

3d2fdc23c7499632afec0c9492fca97fd118967c
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2026-07-03 16:47:41 -0500

    NixOS raspberry pi cachix

--- modules/features/system/nix-wrapper.nix
+++ modules/features/system/nix-wrapper.nix
@@ -17,12 +17,14 @@
           "https://hercules-ci.cachix.org"
           "https://noctalia.cachix.org"
           "https://catppuccin.cachix.org"
+          "https://nixos-raspberrypi.cachix.org"
         ];
         extra-trusted-public-keys = [
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
           "hercules-ci.cachix.org-1:g99HiMR8Y9Z8mYicrKWW04daP0W5ZshhZ/pYgNfC+1U="
           "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
           "catppuccin.cachix.org-1:noGdz9JST3clb8h9W9q6CZvGsDY6HG0cmM6b856w06M="
+          "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
         ];
       };
       extraOptions = ''

6feaf758cc3514527add2534da0d94cf5deec33c
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2026-07-03 20:25:21 -0500

    Build machines mwahah

--- modules/features/system/nix-wrapper.nix
+++ modules/features/system/nix-wrapper.nix
@@ -26,7 +26,10 @@
           "catppuccin.cachix.org-1:noGdz9JST3clb8h9W9q6CZvGsDY6HG0cmM6b856w06M="
           "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
         ];
-        trusted-users = [ "root" "tw1zzler" "@wheel" ];
+        trusted-users = ["root" "tw1zzler" "@wheel"];
+
+        # optional, useful when the builder has a faster internet connection than yours
+        builders-use-substitutes = true;
       };
       extraOptions = ''
         warn-dirty = false
@@ -36,6 +39,39 @@
 
       # Disable channels
       channel.enable = false;
+
+      # You can see the resulting builder-strings of this NixOS-configuration with "cat /etc/nix/machines".
+      # These builder-strings are used by the Nix terminal tool, e.g.
+      # when calling "nix build ...".
+      buildMachines = [
+        {
+          # Will be used to call "ssh builder" to connect to the builder machine.
+          # The details of the connection (user, port, url etc.)
+          # are taken from your "~/.ssh/config" file.
+          hostName = "MALENIA";
+          # CPU architecture of the builder, and the operating system it runs.
+          # Replace the line by the architecture of your builder, e.g.
+          # - Normal Intel/AMD CPUs use "x86_64-linux"
+          # - Raspberry Pi 4 and 5 use  "aarch64-linux"
+          # - M1, M2, M3 ARM Macs use   "aarch64-darwin"
+          # - Newer RISCV computers use "riscv64-linux"
+          # See https://github.com/NixOS/nixpkgs/blob/nixos-unstable/lib/systems/flake-systems.nix
+          # If your builder supports multiple architectures
+          # (e.g. search for "binfmt" for emulation),
+          # you can list them all, e.g. replace with
+          # systems = ["x86_64-linux" "aarch64-linux" "riscv64-linux"];
+          system = "aarch64-linux";
+          # Nix custom ssh-variant that avoids lots of "trusted-users" settings pain
+          protocol = "ssh-ng";
+          # default is 1 but may keep the builder idle in between builds
+          maxJobs = 4;
+          supportedFeatures = ["nixos-test" "benchmark" "big-parallel" "kvm"];
+          sshUser = "nixremote";
+          sshKey = "/root/.ssh/id_ed25519"; # Path to the local root user's private key
+        }
+      ];
+      # required, otherwise remote buildMachines above aren't used
+      distributedBuilds = true;
     };
   };
 }

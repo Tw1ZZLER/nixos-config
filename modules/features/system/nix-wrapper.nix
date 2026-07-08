039b5284d97cf56282c48aeb96f1c62af38d45c4
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2026-07-07 20:10:22 -0500

    Comment out MALENIA build machine until its properly setup later.....

--- modules/features/system/nix-wrapper.nix
+++ modules/features/system/nix-wrapper.nix
@@ -44,31 +44,31 @@
       # These builder-strings are used by the Nix terminal tool, e.g.
       # when calling "nix build ...".
       buildMachines = [
-        {
-          # Will be used to call "ssh builder" to connect to the builder machine.
-          # The details of the connection (user, port, url etc.)
-          # are taken from your "~/.ssh/config" file.
-          hostName = "MALENIA";
-          # CPU architecture of the builder, and the operating system it runs.
-          # Replace the line by the architecture of your builder, e.g.
-          # - Normal Intel/AMD CPUs use "x86_64-linux"
-          # - Raspberry Pi 4 and 5 use  "aarch64-linux"
-          # - M1, M2, M3 ARM Macs use   "aarch64-darwin"
-          # - Newer RISCV computers use "riscv64-linux"
-          # See https://github.com/NixOS/nixpkgs/blob/nixos-unstable/lib/systems/flake-systems.nix
-          # If your builder supports multiple architectures
-          # (e.g. search for "binfmt" for emulation),
-          # you can list them all, e.g. replace with
-          # systems = ["x86_64-linux" "aarch64-linux" "riscv64-linux"];
-          system = "aarch64-linux";
-          # Nix custom ssh-variant that avoids lots of "trusted-users" settings pain
-          protocol = "ssh-ng";
-          # default is 1 but may keep the builder idle in between builds
-          maxJobs = 4;
-          supportedFeatures = ["nixos-test" "benchmark" "big-parallel" "kvm"];
-          sshUser = "nixremote";
-          sshKey = "/root/.ssh/id_ed25519"; # Path to the local root user's private key
-        }
+        # {
+        #   # Will be used to call "ssh builder" to connect to the builder machine.
+        #   # The details of the connection (user, port, url etc.)
+        #   # are taken from your "~/.ssh/config" file.
+        #   hostName = "MALENIA";
+        #   # CPU architecture of the builder, and the operating system it runs.
+        #   # Replace the line by the architecture of your builder, e.g.
+        #   # - Normal Intel/AMD CPUs use "x86_64-linux"
+        #   # - Raspberry Pi 4 and 5 use  "aarch64-linux"
+        #   # - M1, M2, M3 ARM Macs use   "aarch64-darwin"
+        #   # - Newer RISCV computers use "riscv64-linux"
+        #   # See https://github.com/NixOS/nixpkgs/blob/nixos-unstable/lib/systems/flake-systems.nix
+        #   # If your builder supports multiple architectures
+        #   # (e.g. search for "binfmt" for emulation),
+        #   # you can list them all, e.g. replace with
+        #   # systems = ["x86_64-linux" "aarch64-linux" "riscv64-linux"];
+        #   system = "aarch64-linux";
+        #   # Nix custom ssh-variant that avoids lots of "trusted-users" settings pain
+        #   protocol = "ssh-ng";
+        #   # default is 1 but may keep the builder idle in between builds
+        #   maxJobs = 4;
+        #   supportedFeatures = ["nixos-test" "benchmark" "big-parallel" "kvm"];
+        #   sshUser = "nixremote";
+        #   sshKey = "/root/.ssh/id_ed25519"; # Path to the local root user's private key
+        # }
       ];
       # required, otherwise remote buildMachines above aren't used
       distributedBuilds = true;

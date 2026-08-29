387e45378ca3241f1608b2e4cd679cd6b997ffdf
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2026-08-28 20:22:46 -0400

    nextcloud stuff

--- modules/features/services/nextcloud.nix
+++ modules/features/services/nextcloud.nix
@@ -11,15 +11,49 @@
   }: {
     environment.etc."nextcloud-admin-pass".text = "PWD";
 
-    # Ensure firewall allows HTTP and HTTPS traffic
-    networking.firewall.allowedTCPPorts = [80 443];
+    networking.firewall = {
+      trustedInterfaces = ["tailscale0"];
+      allowedUDPPorts = [53];
+      allowedTCPPorts = [53];
+    };
+
+    services.pihole-ftl = {
+      enable = true;
+      settings = {
+        dns = {
+          upstreams = ["1.1.1.1" "8.8.8.8"];
+
+          hosts = [
+            "100.83.191.106 cloud.tw1zzler.net"
+            "100.83.191.106 pihole.tw1zzler.net"
+          ];
+        };
+      };
+    };
+
+    services.pihole-web = {
+      enable = true;
+      ports = [8081];
+      hostName = "pihole.tw1zzler.net";
+    };
+
+    services.nginx.virtualHosts."pihole.tw1zzler.net" = {
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:8081/admin/";
+        proxyWebsockets = true;
+      };
+    };
 
     services.nextcloud = {
       enable = true;
       package = pkgs.nextcloud34;
-      hostName = "localhost";
-      config.adminpassFile = "/etc/nextcloud-admin-pass";
-      config.dbtype = "sqlite";
+
+      hostName = "cloud.tw1zzler.net";
+
+      config = {
+        adminpassFile = "/etc/nextcloud-admin-pass";
+        dbtype = "sqlite";
+      };
 
       extraApps = {
         inherit (config.services.nextcloud.package.packages.apps) news contacts calendar tasks;
@@ -27,11 +61,14 @@
       extraAppsEnable = true;
 
       settings = {
-        # Some sane defaults required to satisfy Nextcloud configuration check
         maintenance_window_start = 1;
         default_phone_region = "DE";
         log_type = "systemd";
         serverid = 0;
+
+        trusted_domains = [
+          "cloud.tw1zzler.net"
+        ];
       };
     };
   };

195a39c1f8716b6c79a9ddbb53b3986e9f889188
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2026-09-17 16:43:23 -0400

    idk why this suddenly changed

--- modules/features/nix-wrapper.nix
+++ modules/features/nix-wrapper.nix
@@ -9,7 +9,7 @@
     nix = {
       settings = {
         # Enable flakes and new 'nix' command
-        experimental-features = "nix-command flakes";
+        experimental-features = ["nix-command" "flakes"];
 
         # Extra Binary Caches
         extra-substituters = [
